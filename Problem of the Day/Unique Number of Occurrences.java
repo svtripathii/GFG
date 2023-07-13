e9279dcc5e07a63e0e40775a4507e985f70f4179
Saurav Tripathi <95672002+svtripathii@users.noreply.github.com>
2023-07-13 15:22:03 +0530

    Create Unique Number of Occurrences.java

---
+++ Problem of the Day/Unique Number of Occurrences.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+
+class IntArray
+{
+    public static int[] input(BufferedReader br, int n) throws IOException
+    {
+        String[] s = br.readLine().trim().split(" ");
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++)
+            a[i] = Integer.parseInt(s[i]);
+
+        return a;
+    }
+
+    public static void print(int[] a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+
+    public static void print(ArrayList<Integer> a)
+    {
+        for(int e : a)
+            System.out.print(e + " ");
+        System.out.println();
+    }
+}
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t;
+        t = Integer.parseInt(br.readLine());
+        while(t-- > 0){
+            
+            int n;
+            n = Integer.parseInt(br.readLine());
+            
+            
+            int[] arr = IntArray.input(br, n);
+            
+            Solution obj = new Solution();
+            boolean res = obj.isFrequencyUnique(n, arr);
+            
+            int _result_val = (res) ? 1 : 0;
+            System.out.println(_result_val);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    public static boolean isFrequencyUnique(int n, int[] arr) {
+        // code here
+        Map<Integer,Integer>mp = new HashMap<>();
+        for(int x:arr)
+        mp.put(x,mp.getOrDefault(x,0)+1);
+        Set<Integer>set = new HashSet<>();
+        
+        for(int i:mp.keySet()){
+            if(!set.contains(mp.get(i)))
+            set.add(mp.get(i));
+            else
+            return false;
+        }
+        return true;
+    }
+}
+        

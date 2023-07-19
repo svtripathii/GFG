c8cda602820521d99df2b44eaddc775a2ac8205e
Saurav Tripathi <95672002+svtripathii@users.noreply.github.com>
2023-07-19 17:24:19 +0530

    Create Longest Palindromic Subsequence.java

---
+++ Problem of the Day/Longest Palindromic Subsequence.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s = sc.next();
+                    Solution obj = new Solution();
+                    System.out.println(obj.longestPalinSubseq(s));
+                }
+                
+        }
+}
+// } Driver Code Ends
+
+class Solution
+{
+    public int longestPalinSubseq(String S)
+    {
+        //code here
+        String P = new StringBuilder(S).reverse().toString();
+        // Dp[i][i] will store the length of the longest.
+        // palindromic subsequence for the substring.
+        // starting at index i and ending at index j
+        
+        int dp[][] = new int[S.length() + 1][P.length() + 1];
+        
+        // Filling up Dp table based on conditions discussed
+        
+        for (int i=1; i<=S.length(); i++) {
+            for (int j = 1; j <= P.length(); j++) {
+                if(S.charAt(i- 1) == P.charAt(j-1))
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+                else 
+                dp[i][j] = Math.max(dp[i][j-1], dp[i - 1][j]);
+            }
+        }
+        // At the end dp table will contain the LPS So just return the Length of LPS.
+        return dp[S.length()][P.length()];
+        
+    }
+}

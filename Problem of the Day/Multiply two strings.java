b044a0f9d9a6e0065809094b0da613faaa5b27aa
Saurav Tripathi <95672002+svtripathii@users.noreply.github.com>
2023-08-24 20:20:32 +0530

    Create Multiply two strings.java

---
+++ Problem of the Day/Multiply two strings.java
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+import java.util.*;
+import java.math.*;
+
+class Multiply{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            String a=sc.next();
+            String b=sc.next();
+            Solution g=new Solution();
+            System.out.println(g.multiplyStrings(a,b));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    public String multiplyStrings(String s1,String s2)
+    {
+        //code here.
+    BigInteger a= new BigInteger(s1);
+    BigInteger b=  new BigInteger(s2);
+    b=b.multiply(a);
+    return b.toString();
+    }
+}
